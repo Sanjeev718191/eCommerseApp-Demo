bbc1b7266ee0fc31e0532668b5a676513ffce905
Sanjeev <sanjeev718191@gmail.com>
2023-01-24 23:47:48 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/ecommerseapp/Prevalent/Prevalent.java
@@ -0,0 +1,9 @@
+package com.example.ecommerseapp.Prevalent;
+
+import com.example.ecommerseapp.Model.Users;
+
+public class Prevalent {
+    private static Users currentOnlineUser;
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
